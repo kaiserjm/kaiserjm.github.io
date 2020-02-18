def10110275a0a8f40ab78375a1c0c2039105075
Andrew Thompson <thomp664@uwm.edu>
2020-02-18 13:33:57 -0600

    Create custom.js

---
+++ scripts/custom.js
@@ -0,0 +1,26 @@
+var times = [
+  {singular: "year", plural: "years", f: "years"},
+  {singular: "month", plural: "months", f: "months"},
+  {singular: "day", plural: "days", f: "days"},
+  {singular: "hour", plural: "hours", f: "hours"},
+  {singular: "minute", plural: "minutes", f: "minutes"}
+]
+
+
+$(document).ready(function() {
+  var age = document.getElementById("age");
+  let iWasBorn = moment.tz("1995-06-18 00:38", "America/Chicago");
+  let setAge = () => {
+    let now = moment()
+    let diff = now.diff(iWasBorn);
+    var duration = moment.duration(diff);
+    age.innerText = times.map(x => {
+      let count = duration[x.f]();
+      if(count === 0) return null;
+      if(count === 1) return `${count} ${x.singular}`;
+      return `${count} ${x.plural}`;
+    }).filter(x => x).join(", ");
+  }
+  setAge();
+  setInterval(setAge, 2000);
+});
